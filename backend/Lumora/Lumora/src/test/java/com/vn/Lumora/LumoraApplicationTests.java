a17e65868fcdd6413ef472a5cc3927eb8238dc0d
Bao-Nhung <nguyengiabaoo2008@gmail.com>
2026-07-02 23:23:17 +0700

    first commit

---
+++ backend/Lumora/Lumora/src/test/java/com/vn/Lumora/LumoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.Lumora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
